840e56f83dc76d6fb180bc08f05ae2aa1196c641
NaelLich <necromancer.path@gmail.com>
2025-02-20 23:43:08 +0100

    Updated main.py, working on count alphabet

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        counter = 0
-        file_contents = f.read()
-        print(len(file_contents.split()))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_characters = count_characters(text)
+    print(f"{num_words} words found in the document")
+    
 
-if __name__ == "__main__":
-    main()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_characters(text):
+    characters_dict = {}
+    for character in text.lower():
+        if not character in characters_dict:
+            characters_dict[character] = 0
+        if character in characters_dict:
+            characters_dict[character] += 1
+    return characters_dict
+
+def count_alphabet(text):
+    characters_dict = count_characters(text)
+    character_list = []
+    for char, count in characters_dict:
+
+
+
+    
+
+main()
