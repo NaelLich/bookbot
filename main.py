6a657ad47bc5876fa9aff56b1fc86388eff445f0
NaelLich <necromancer.path@gmail.com>
2025-02-21 23:16:58 +0100

    Ended the project!

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from stats import get_num_words, count_characters, get_book_text, count_alphabet
+from stats import get_num_words, get_chars_dict, count_alpha_characters
+import sys
 
 def main():
+    if len(sys.argv) == 1:
+        print("Please provide path to the book")
+        sys.exit(1)
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    text = open(sys.argv[1]).read()
     num_words = get_num_words(text)
-    num_characters = count_characters(text)
-    print(f"{num_words} words found in the document")
+    num_characters = get_chars_dict(text)
+    alpha_characters = count_alpha_characters(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in alpha_characters:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
     
-
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
 main()  
--- stats.py
+++ stats.py
@@ -3,20 +3,27 @@ def get_num_words(text):
     return len(words)
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-def count_characters(text):
+def get_chars_dict(text):
     characters_dict = {}
     for character in text.lower():
         if not character in characters_dict:
-            characters_dict[character] = 0
-        if character in characters_dict:
+            characters_dict[character] = 1
+        else:
             characters_dict[character] += 1
     return characters_dict
 
+def sort_on(dict):
+    return dict["num"]
 
-def count_alphabet(text):
-    characters_dict = count_characters(text)
-    print(characters_dict.items())
+
+def count_alpha_characters(text):
+    characters_dict = get_chars_dict(text)
+    characters_list = []
+    for char, count in characters_dict.items():
+        if char.isalpha():
+            temps_dic = {"char": char, "num": count}
+            characters_list.append(temps_dic)
+    characters_list.sort(reverse = True, key=sort_on)
+    return characters_list
+
+
